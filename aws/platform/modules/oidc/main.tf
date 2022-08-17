aec4e369d8aa062e2f30fac795d52f21c081af41
Katsuhiro Yamanaka <29446925+ogis-yamanaka@users.noreply.github.com>
2022-08-17 10:34:34 +0900

    OIDC対応 nautible/issues#68

--- aws/platform/modules/oidc/main.tf
+++ aws/platform/modules/oidc/main.tf
@@ -70,3 +70,88 @@ resource "aws_iam_role_policy" "githubactions_ecr_access_role_policy" {
 }
 POLICY
 }
+
+resource "aws_iam_role" "githubactions_infra_role" {
+  name = "${var.pjname}-githubactions-infra-role"
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/ReadOnlyAccess"
+  ]
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Principal": {
+        "Federated": "${aws_iam_openid_connect_provider.oidc_provider.id}"
+      },
+      "Condition": {
+        "StringLike": {
+          "token.actions.githubusercontent.com:sub": [
+            "repo:${var.oidc.github_organization}/*"
+          ]
+        }
+      }
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "githubactions_infra_role_policy" {
+  name = "${aws_iam_role.githubactions_infra_role.name}-policy"
+  role = aws_iam_role.githubactions_infra_role.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "sts:GetServiceBearerToken",
+        "ec2:*",
+        "elasticloadbalancing:*",
+        "s3:*",
+        "ecr:*",
+        "eks:*",
+        "cloudfront:*",
+        "sqs:*",
+        "sns:*",
+        "rds:*",
+        "dynamodb:*",
+        "elasticache:*",
+        "route53:*",
+        "acm:*",
+        "cloudwatch:*",
+        "logs:*",
+        "iam:CreatePolicy",
+        "iam:DetachRolePolicy",
+        "iam:DeleteRolePolicy",
+        "iam:DeletePolicy",
+        "iam:CreateRole",
+        "iam:DeleteRole",
+        "iam:AttachRolePolicy",
+        "iam:UpdateRole",
+        "iam:PutRolePolicy",
+        "iam:TagRole",
+        "iam:TagPolicy",
+        "iam:CreateUser"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": "iam:PasRole",
+      "Resource": "*",
+      "Condition": {
+        "StringEquals": {
+          "iam:PassToService": "eks.amazonaws.com"
+        }
+      }
+    }
+  ]
+}
+POLICY
+}
