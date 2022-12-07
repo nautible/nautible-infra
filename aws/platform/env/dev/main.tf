26506449260c1c913c26331cc26c2c31e1f098f6
Katsuhiro Yamanaka <29446925+ogis-yamanaka@users.noreply.github.com>
2022-12-07 17:30:52 +0900

    backendを引数に変更

--- aws/platform/env/dev/main.tf
+++ aws/platform/env/dev/main.tf
@@ -5,12 +5,12 @@ provider "aws" {
 terraform {
   # fix folloing value
   backend "s3" {
-    bucket  = "nautible-dev-platform-tf-ap-northeast-1"
-    region  = "ap-northeast-1"
-    key     = "nautible-dev-platform.tfstate"
-    encrypt = true
-    # if you don't need to dynamodb tfstate lock, comment out this line.
-    dynamodb_table = "nautible-dev-platform-tfstate-lock"
+    # bucket  = "nautible-dev-platform-tf-ap-northeast-1"
+    # region  = "ap-northeast-1"
+    # key     = "nautible-dev-platform.tfstate"
+    # encrypt = true
+    # # if you don't need to dynamodb tfstate lock, comment out this line.
+    # dynamodb_table = "nautible-dev-platform-tfstate-lock"
   }
 
   required_providers {
