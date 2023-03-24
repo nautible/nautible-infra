748c59d1ac6b44ccc9ca2e9197aed34b6f2d0855
Katsuhiro Yamanaka <29446925+ogis-yamanaka@users.noreply.github.com>
2023-03-24 10:34:00 +0900

    terraform fmt

--- azure/plugin/modules/backup/env/dev/variables.tf
+++ azure/plugin/modules/backup/env/dev/variables.tf
@@ -8,16 +8,16 @@ variable "location" {
 
 variable "backup_resource_group_name" {
   description = "バックアップ関連のリソースをまとめたリソースグループ"
-  default = "nautibledevbackup"
+  default     = "nautibledevbackup"
 }
 
 variable "backup_storage_account_name" {
   description = "バックアップデータ用ストレージアカウント"
-  default = "nautibledevbackup"
+  default     = "nautibledevbackup"
 }
 
 variable "backup_storage_container_name" {
   description = "バックアップデータ用コンテナ"
-  default = "nautibledevbackupcontainer"
+  default     = "nautibledevbackupcontainer"
 }
 
