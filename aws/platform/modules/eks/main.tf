ce76913a15882a1a67c118244070891eeb6c8aa2
Katsuhiro Yamanaka <29446925+ogis-yamanaka@users.noreply.github.com>
2023-03-30 10:15:20 +0900

    --max-pods=110

--- aws/platform/modules/eks/main.tf
+++ aws/platform/modules/eks/main.tf
@@ -51,6 +51,16 @@ module "eks" {
         delete_on_termination = true
       }
     ]
+    pre_bootstrap_user_data = <<-EOT
+      #!/bin/bash
+      set -ex
+      cat <<-EOF > /etc/profile.d/bootstrap.sh
+      export USE_MAX_PODS=false
+      export KUBELET_EXTRA_ARGS="--max-pods=110"
+      EOF
+      # Source extra environment variables in bootstrap script
+      sed -i '/^set -o errexit/a\\nsource /etc/profile.d/bootstrap.sh' /etc/eks/bootstrap.sh
+    EOT
   }
 
   eks_managed_node_groups = {
