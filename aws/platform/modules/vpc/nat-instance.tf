5f94ecf3878a870546a98a0e6824d0ede1d37bfa
Katsuhiro Yamanaka <29446925+ogis-yamanaka@users.noreply.github.com>
2024-03-15 13:38:13 +0900

    nat-instanceの設定変更 明示的にpublic_ipをアサインする必要あり

--- aws/platform/modules/vpc/nat-instance.tf
+++ aws/platform/modules/vpc/nat-instance.tf
@@ -2,16 +2,16 @@
 module "nat_instance" {
   count   = var.nat_instance_type == null ? 0 : 1
   source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "2.17.0"
+  version = "5.6.0"
 
-  name                   = "${var.pjname}-nat-instance"
-  instance_count         = 1
-  ami                    = data.aws_ami.nat_ami_recent[0].id
-  instance_type          = var.nat_instance_type
-  monitoring             = true
-  source_dest_check      = false
-  vpc_security_group_ids = [module.nat_instance_security_group[0].security_group_id]
-  subnet_id              = module.vpc.public_subnets[0]
+  name                        = "${var.pjname}-nat-instance"
+  ami                         = data.aws_ami.nat_ami_recent[0].id
+  instance_type               = var.nat_instance_type
+  monitoring                  = true
+  source_dest_check           = false
+  vpc_security_group_ids      = [module.nat_instance_security_group[0].security_group_id]
+  subnet_id                   = module.vpc.public_subnets[0]
+  associate_public_ip_address = true
 
   tags = {
     Name = "${var.pjname}-nat-instance"
