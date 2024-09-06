01e2e8e4cb4ac999742bfd3fbc51ce8c03d24f7c
Katsuhiro Yamanaka <29446925+ogis-yamanaka@users.noreply.github.com>
2024-09-06 16:30:31 +0900

    未使用output削除

--- aws/platform/outputs.tf
+++ aws/platform/outputs.tf
@@ -18,11 +18,6 @@ output "eks" {
         role_name         = v.node_role_name
         security_group_id = v.node_security_group_id
       }
-      albc = {
-        role_arn            = v.albc_role_arn
-        security_group_id   = v.albc_security_group_id
-        security_group_name = v.albc_security_group_name
-      }
       oidc = {
         provider_arn = v.oidc_provider_arn
       }
