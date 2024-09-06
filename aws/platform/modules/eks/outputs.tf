7a48660b0dfdd973afa23cb625f2cbcc443b5da4
Katsuhiro Yamanaka <29446925+ogis-yamanaka@users.noreply.github.com>
2024-09-06 16:33:05 +0900

    未使用output削除

--- aws/platform/modules/eks/outputs.tf
+++ aws/platform/modules/eks/outputs.tf
@@ -17,14 +17,3 @@ output "node_security_group_id" {
 output "oidc_provider_arn" {
   value = module.eks.oidc_provider_arn
 }
-
-output "albc_role_arn" {
-  value = module.load_balancer_controller_irsa_role.iam_role_arn
-}
-
-output "albc_security_group_id" {
-  value = module.albc_security_group.security_group_id
-}
-output "albc_security_group_name" {
-  value = module.albc_security_group.security_group_name
-}
