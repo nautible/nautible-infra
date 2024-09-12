cee857cd6985409ccbdb2a095a460834c4fdffbf
Katsuhiro Yamanaka <29446925+ogis-yamanaka@users.noreply.github.com>
2024-09-12 16:34:47 +0900

    initの構成見直し nautible/issues#160

---
+++ aws/init/variables.tf
@@ -0,0 +1,18 @@
+variable "pjname" {
+  description = "プロジェクト名称"
+}
+
+variable "environment" {
+  description = "環境名定義"
+  default     = "dev"
+}
+
+variable "use_lock_table" {
+  description = "ロックテーブル（DynamoDB）の利用有無"
+  default     = true
+}
+
+# aws region 
+variable "region" {
+  default = "ap-northeast-1"
+}
